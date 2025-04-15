97a540696d1fef75176961a8a35ed2e0eab28ea4
DeAcTv8 <deandre.courtney@gmail.com>
2025-04-15 13:54:39 -0500

    Initial API project setup by Zen

---
+++ CmdShiftLearn.Api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.MapGet("/", () => "CmdShiftLearn API is working!");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Add health endpoint
+app.MapGet("/health", () => "OK");
+
+app.Run();
